217ada3059e4ddc68a78db1798a648837d68bc28
tracer-ics2017 <tracer@njuics.org>
2020-03-06 21:03:14 +0800

    >  compile 1410576 qc Linux qc-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  21:03:14 up 53 min,  1 user,  load average: 0.36, 0.19, 0.23 88ea3f5c94f26ab388152f82e07dbc2675191b2b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -77,7 +77,7 @@ static bool make_token(char *e) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+        printf("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
         position += substr_len;
 
@@ -121,7 +121,7 @@ uint32_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  //TODO();
+  TODO();
 
   return 0;
 }
