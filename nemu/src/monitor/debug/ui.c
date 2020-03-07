ed29421f2b464f275691ab63697fc40ba62df1ce
tracer-ics2017 <tracer@njuics.org>
2020-03-07 13:44:16 +0800

    >  compile 1410576 qc Linux qc-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  13:44:16 up  2:47,  1 user,  load average: 0.33, 0.31, 0.27 3db60549f1adaa15640427392d90779782d3f9bc

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -107,7 +107,7 @@ static int cmd_x(char *args)
   //int nRet = sscanf(args, "%d  0x%x", &nLen, &addr);
   char *arg = NULL;
   int nRet = sscanf(args, "%d  %s", &nLen, arg);
-  printf("%s\n", arg);
+  printf("%d, %s\n", nLen, arg);
   if (nRet <= 0)
   {
     printf("args error in cmd_x\n");
