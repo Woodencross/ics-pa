2d274b82491294fa7198c6f7e6a172eba821f8e8
tracer-ics2017 <tracer@njuics.org>
2020-04-26 11:52:03 +0800

    >  compile 1410576 qc Linux qc-virtual-machine 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:01:48 UTC 2020 i686 i686 i686 GNU/Linux  11:52:03 up  1:29,  1 user,  load average: 0.59, 0.32, 0.21 99d51626e12e06159ef37cb490219a13417c47af

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -22,8 +22,6 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
       break;
     case CC_E:
       rtl_get_ZF(dest);
-    case CC_NE:
-      //*dest=cpu.eflags.ZF!=1?1:0;
       break;
     case CC_BE:
       assert(dest!=&t0);
@@ -33,6 +31,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
       break;
     case CC_S:
       rtl_get_SF(dest);
+      break;
     case CC_L:
       assert(dest!=&t0);
       rtl_get_SF(dest);
@@ -46,6 +45,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
       rtl_xor(dest,dest,&t0);
       rtl_get_ZF(&t0);
       rtl_or(dest,dest,&t0);
+      break;
       //TODO();
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
