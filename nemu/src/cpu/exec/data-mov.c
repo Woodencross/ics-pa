89256d49e39fe17aaae74d5b2b39357852544584
tracer-ics2017 <tracer@njuics.org>
2020-04-26 23:47:46 +0800

    >  compile 1410576 qc Linux qc-virtual-machine 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:01:48 UTC 2020 i686 i686 i686 GNU/Linux  23:47:46 up  2:56,  1 user,  load average: 0.28, 0.46, 0.34 2f57dd319b000400d4d21b1fb44cf72ed9366973

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -57,16 +57,13 @@ make_EHelper(cltd) {
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
     //TODO();
-    rtl_lr_b(&t0,R_AL);
-    rtl_sext(&t0,&t0,1);
-    rtl_sari(&t0,&t0,31);
-    rtl_sr_w(R_AX,&t0);
+    rtl_sext(&t0,&cpu.eax,1);
+    cpu.eax=(cpu.eax&0xffff0000)|(t0&0xffff);
   }
   else {
     //TODO();
-    rtl_lr_w(&t0,R_AX);
-    rtl_sext(&t0,&t0,2);
-    rtl_sari(&cpu.eax,&t0,31);
+    rtl_sext(&t0,&cpu.eax,2);
+    cpu.eax=t0;
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
