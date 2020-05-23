c0a82a713c68de62fb70a6df9115db582b8baf09
tracer-ics2017 <tracer@njuics.org>
2020-05-24 00:01:50 +0800

    >  run 1410576 qc Linux qc-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  00:01:50 up  1:25,  1 user,  load average: 0.02, 0.16, 0.15 48a49679c5d6fc389e0af1583f4ceb85163dab26

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,11 @@
 #include "common.h"
 
+//注册外部引用
+extern _RegSet* do_syscall(_RegSet *r);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
+    case _EVENT_SYSCALL:
+    return do_syscall(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
 
