879e21fc8962977d597a2039a1829768a4994f85
tracer-ics2017 <tracer@njuics.org>
2020-06-10 21:56:19 +0800

    >  run 1410576 qc Linux qc-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  21:56:19 up  5:22,  1 user,  load average: 0.04, 0.13, 0.28 41d86a6785066dcc6f2d17ff77cf6c74c070d0a2

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -31,7 +31,9 @@ int sys_write(int fd,void* buf,int len){
 }
 
 int sys_brk(int addr){
-  return 0;
+  //return 0;
+  extern int mm_brk(uint32_t new_brk);
+  return mm_brk(addr);
 }
 
 int sys_open(const char* filename){
