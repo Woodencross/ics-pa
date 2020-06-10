8f509429560f4d78d23ee3d725d911c738f0e587
tracer-ics2017 <tracer@njuics.org>
2020-06-10 17:48:42 +0800

    >  run 1410576 qc Linux qc-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  17:48:42 up  1:15,  1 user,  load average: 0.18, 0.19, 0.18 dec87ba470ab1f2224501624355e873d84abd456

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,8 +30,8 @@ int main() {
 
   init_fs();
 
-  uint32_t entry = loader(NULL, "/bin/pal");
-  ((void (*)(void))entry)();
+  extern void load_prog(const char* filename);
+  load_prog("/bin/dummy");
 
   panic("Should not reach here");
 }
