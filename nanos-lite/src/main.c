62a74e7ad41653a6ad0c528025b1fa9be33e0405
tracer-ics2017 <tracer@njuics.org>
2020-06-10 21:36:26 +0800

    >  compile 1410576 qc Linux qc-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  21:36:26 up  5:02,  1 user,  load average: 0.64, 0.70, 0.44 7f260ddff16f669097f473d6b0b6ce9644b5972

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -33,7 +33,7 @@ int main() {
   //uintptr_t entry = loader(NULL, "/bin/hello");
   //((void(*)(void))entry)();
   extern void load_prog(const char* filename);
-  load_prog("/bin/hello");
+  load_prog("/bin/dummy");
 
   panic("Should not reach here");
 }
