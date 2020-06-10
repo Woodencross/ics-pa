786dbabf80315e7b5907828d13e92c2b110fcc7f
tracer-ics2017 <tracer@njuics.org>
2020-06-10 14:01:26 +0800

    >  compile 1410576 qc Linux qc-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  14:01:26 up  3:06,  1 user,  load average: 0.35, 0.25, 0.19 d02b8d503f64c45b8d322808839efde21674e80a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,6 +1,47 @@
 #include "nemu.h"
 #include "device/mmio.h"
 
+//PA4 page translate start
+
+#include "memory/mmu.h"
+
+#define PTE_ADDR(pte)    ((uint32_t)(pte)&~0xfff)
+#define PDX(va)          (((uint32_t)(va)>>22)&0x3fff)
+#define PTX(va)          (((uint32_t)(va)>>12)&0x3fff)
+#define OFF(va)          ((uint32_t)(va)&0xfff)
+
+paddr_t page_translate(vaddr_t addr,bool iswrite){
+  CR0 cr0=(CR0)cpu.CR0;
+  if(cr0.paging && cr0.protect_enable){
+    CR3 cr3=(CR3)cpu.CR3;
+
+    //页目录表
+    PDE* pgdirs=(PDE*)PTE_ADDR(cr3.val);
+    PDE pde=(PDE)paddr_read((uint32_t)(pgdirs+PDX(addr)),4);
+    Assert(pde.present,"addr=0x%x",addr);
+
+    //二级页表
+    PTE* ptab=(PTE*)PTE_ADDR(pde.val);
+    PTE pte=(PTE)paddr_read((uint32_t)(ptab+PTX(addr)),4);
+    Assert(pte.present,"addr=0x%x",addr);
+
+    //设置accessed与dirty
+    pde.accessed=1;
+    pte.accessed=1;
+    if(iswrite){
+      pte.dirty=1;
+    }
+
+    //计算物理地址
+    paddr_t paddr=PTE_ADDR(pte.val) | OFF(addr);
+    return paddr;
+  }
+  printf("return paddr = vaddr\n");
+  return addr;
+}
+
+//PA4 page translate end
+
 #define PMEM_SIZE (128 * 1024 * 1024)
 
 #define pmem_rw(addr, type) *(type *)({\
@@ -34,9 +75,25 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  //return paddr_read(addr, len);
+  if(PTE_ADDR(addr)!=PTE_ADDR(addr+len-1)){//页基址不同说明跨页面
+    printf("error: the data pass two pages: addr = 0x%x, len = %d!\n",addr,len);
+    assert(0);
+  }
+  else{
+    paddr_t paddr=page_translate(addr,false);
+    return paddr_read(paddr,len);
+  }
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  //paddr_write(addr, len, data);
+  if(PTE_ADDR(addr)!=PTE_ADDR(addr+len-1)){
+    printf("error: the data pass two pages: addr = 0x%x, len = %d!\n",addr,len);
+    assert(0);
+  }
+  else{
+    paddr_t paddr=page_translate(addr,true);
+    paddr_write(paddr,len,data);
+  }
 }
